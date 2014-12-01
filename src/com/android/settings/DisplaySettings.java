54eac04c5fd64e2cbf93185fb1af0293135c9f5c
Danesh M <daneshm90@gmail.com>
2014-12-01 09:29:57 -0600

    Settings: Add proximity wake option
    Ports and combines:
    
    http://review.cyanogenmod.org/#/c/66658/
    Settings : Add preference for proximity wake
    
    http://review.cyanogenmod.org/#/c/69347/
    Proximity Wake-Up : adjust strings
    
    http://review.cyanogenmod.org/#/c/69803/
    Settings : Make proximity opt-in
    
    Change-Id: Iea0853eccf764438c7aaac76afd13c364fcea0ec
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -72,6 +72,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_AUTO_BRIGHTNESS = "auto_brightness";
     private static final String KEY_AUTO_ROTATE = "auto_rotate";
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
+    private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -179,6 +180,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                         Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
                 mVolumeWake.setOnPreferenceChangeListener(this);
             }
+        }
+
+        boolean proximityCheckOnWait = getResources().getBoolean(
+                com.android.internal.R.bool.config_proximityCheckOnWake);
+        if (!proximityCheckOnWait) {
+            advancedPrefs.removePreference(findPreference(KEY_PROXIMITY_WAKE));
+            Settings.System.putInt(getContentResolver(), Settings.System.PROXIMITY_ON_WAKE, 1);
         }
     }
 
