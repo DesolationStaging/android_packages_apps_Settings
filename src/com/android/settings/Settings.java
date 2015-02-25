475739566f4e04fead634f52069a0a502632e3b7
REV3NT3CH <rev3nt3ch@gmail.com>
2015-02-24 18:25:52 -0800

    Add Custom Desolation Settings
    Also Add Desolation ROM Logo to about phone section

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -101,6 +101,6 @@ public class Settings extends SettingsActivity {
 
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
-    public static class PlainTweakInfo extends SettingsActivity { /* empty */ }
+    public static class CustomSettingsActivity extends SettingsActivity { /* empty */ }
 }
 
