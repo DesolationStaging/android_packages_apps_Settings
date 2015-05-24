SlimRoms/packages_apps_Settings

[2/2] Custom QS
Add customization for Quick Settings [2/2]

Add a new section to customize Quick Settings.

Modified DraggableGridView to arrange the items differently.

Gave everything a "Material-ish" style.

Inversion and Hotspot tiles can be rearranged now.

Creadted a "Notification Drawer" category.

frameworks/base: http://review.cyanogenmod.org/83417/

Conflicts:
	res/values/cm_strings.xml
	res/values/dimens.xml
	res/xml/dashboard_categories.xml

QS: change location sensors mode in quick settings [2/2]

Added switch in notification drawer settings
to enable advanced setting for location tile

Change-Id: Id395a93452e81b9526d98bb8c683322eef6dc4b2

Conflicts:
	res/values/cm_strings.xml
	res/xml/notification_drawer_settings.xml

Fix bug on QS selection preference.

DraggableGridView allows you to put a tile after the Add/Delete one, causing buggy behaviour.

Change-Id: Id0c61b188ce72d3bf371fd906ecd47c41c7ae528

Quick settings: Option to disable brightness slider (2/2)

Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>

Change-Id: I0440351bc88ddcd30d2e9d22f14e3a04406ba3f3

Conflicts:
	res/values/cm_strings.xml

Settings: Update QS_TILES to point at secure settings.

Change-Id: I600701b0c9f71f6569743ac5ae18bf5c96fe5b0a

Settings: Move qs brightness slider option to secure

Change-Id: I246a06ead789d921b19b64a71127491546ae158e

Conflicts:
	res/xml/qs_settings.xml

Cleanup and fix QS tile implementation

- Decouple DraggableGridView and QSTiles code by shifting around some
  logic
- Fix column-for-position calculation
- Improve layouts and preference ordering

Change-Id: I7a06150f2602888b0f28fca93e0164e381272b67

Conflicts:
	res/xml/qs_settings.xml

Settings : Update for centralized quick settings

Change-Id: If9cad4fd45e5bf7deb13aa1a5284ac5566e44a95

Conflicts:
	res/values/cm_colors.xml
	src/com/android/settings/Utils.java
	src/com/android/settings/slim/qs/QSTileHolder.java

Cleanup.

Change-Id: Iab641daadf9e11ee41d6afdefe7476fb7f490ddd

Conflicts:
	src/com/android/

src/com/android/settings/slim/qs/ColoringCardView.java
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.slim.qs;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class ColoringCardView extends CardView {
+    private PorterDuffColorFilter mColorFilter;
+    private Paint mPaint;
+
+    public ColoringCardView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ColoringCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ColoringCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setColor(int color) {
+        if (color != mColorFilter.getColor()) {
+            mColorFilter.setColor(color);
+            mPaint.setColorFilter(color == Color.TRANSPARENT ? null : mColorFilter);
+            setLayerPaint(mPaint);
+        }
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mColorFilter = new PorterDuffColorFilter(Color.TRANSPARENT, PorterDuff.Mode.MULTIPLY);
+        setLayerType(LAYER_TYPE_HARDWARE, mPaint);
+    }
+}
+

src/com/android/settings/slim/qs/QSListAdapter.java
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.slim.qs;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+
+import java.util.List;
+
+public class QSListAdapter extends ArrayAdapter<QSTileHolder> {
+    private Context mSystemUiContext;
+    private LayoutInflater mInflater;
+
+    public QSListAdapter(Context context, Context systemUiContext, List<QSTileHolder> tiles) {
+        super(context, 0, tiles);
+        mInflater = LayoutInflater.from(context);
+        mSystemUiContext = systemUiContext;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Holder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.add_qs_list_item, parent, false);
+            holder = new Holder();
+            holder.entry = (TextView) convertView.findViewById(android.R.id.text1);
+            holder.icon = (ImageView) convertView.findViewById(android.R.id.icon);
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder) convertView.getTag();
+        }
+
+        QSTileHolder item = getItem(position);
+
+        holder.entry.setText(item.name);
+        if (item.resourceName != null) {
+            Drawable d = Utils.getNamedDrawable(mSystemUiContext, item.resourceName);
+            d.setColorFilter(getContext().getResources().getColor(R.color.qs_tile_tint_color),
+                    PorterDuff.Mode.SRC_ATOP);
+            holder.icon.setImageDrawable(d);
+            holder.icon.setVisibility(View.VISIBLE);
+        } else {
+            holder.icon.setVisibility(View.GONE);
+        }
+
+        return convertView;
+    }
+
+    private static class Holder {
+        public TextView entry;
+        public ImageView icon;
+    }
+}

DesolationStaging/android_packages_apps_Settings

src/com/android/settings/desolation/qs/ColoringCardView.java
@@ -1,0 +1,59 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.desolation.qs;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class ColoringCardView extends CardView {
+    private PorterDuffColorFilter mColorFilter;
+    private Paint mPaint;
+
+    public ColoringCardView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ColoringCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ColoringCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setColor(int color) {
+        if (color != mColorFilter.getColor()) {
+            mColorFilter.setColor(color);
+            mPaint.setColorFilter(color == Color.TRANSPARENT ? null : mColorFilter);
+            setLayerPaint(mPaint);
+        }
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mColorFilter = new PorterDuffColorFilter(Color.TRANSPARENT, PorterDuff.Mode.MULTIPLY);
+        setLayerType(LAYER_TYPE_HARDWARE, mPaint);
+    }
+}

src/com/android/settings/desolation/qs/QSListAdapter.java
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.desolation.qs;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+
+import java.util.List;
+
+public class QSListAdapter extends ArrayAdapter<QSTileHolder> {
+    private Context mSystemUiContext;
+    private LayoutInflater mInflater;
+
+    public QSListAdapter(Context context, Context systemUiContext, List<QSTileHolder> tiles) {
+        super(context, 0, tiles);
+        mInflater = LayoutInflater.from(context);
+        mSystemUiContext = systemUiContext;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Holder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.add_qs_list_item, parent, false);
+            holder = new Holder();
+            holder.entry = (TextView) convertView.findViewById(android.R.id.text1);
+            holder.icon = (ImageView) convertView.findViewById(android.R.id.icon);
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder) convertView.getTag();
+        }
+
+        QSTileHolder item = getItem(position);
+
+        holder.entry.setText(item.name);
+        if (item.resourceName != null) {
+            Drawable d = Utils.getNamedDrawable(mSystemUiContext, item.resourceName);
+            d.setColorFilter(getContext().getResources().getColor(R.color.qs_tile_tint_color),
+                    PorterDuff.Mode.SRC_ATOP);
+            holder.icon.setImageDrawable(d);
+            holder.icon.setVisibility(View.VISIBLE);
+        } else {
+            holder.icon.setVisibility(View.GONE);
+        }
+
+        return convertView;
+    }
+
+    private static class Holder {
+        public TextView entry;
+        public ImageView icon;
+    }
+}